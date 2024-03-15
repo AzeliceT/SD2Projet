b701e0e4c94c43a01179bf5e45e1a5b847fef92b
ana.araujo <ana.araujo@student.vinci.be>
2024-03-15 14:54:20 +0100

    Premier essaie pr la méthode ' calculerItineraireMinimisantNombreRoute '

--- src/Graph.java
+++ src/Graph.java
@@ -2,8 +2,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Graph {
@@ -27,7 +29,8 @@ public class Graph {
         String cityData = cityScanner.nextLine();
         String[] cityDataArray = cityData.split(",");
 
-        City city = new City(Integer.parseInt(cityDataArray[0]), cityDataArray[1],Double.parseDouble(cityDataArray[2]),  Double.parseDouble(cityDataArray[3]));
+        City city = new City(Integer.parseInt(cityDataArray[0]), cityDataArray[1],
+            Double.parseDouble(cityDataArray[2]), Double.parseDouble(cityDataArray[3]));
         correspondanceIataCity.put(cityDataArray[0], city);
 
       }
@@ -43,7 +46,8 @@ public class Graph {
         String roadData = roadScanner.nextLine();
         String[] roadDataArray = roadData.split(",");
 
-        Road road = new Road(Integer.parseInt(roadDataArray[0]), Integer.parseInt(roadDataArray[1]));
+        Road road = new Road(Integer.parseInt(roadDataArray[0]),
+            Integer.parseInt(roadDataArray[1]));
         roadsList.add(road);
 
       }
@@ -53,13 +57,44 @@ public class Graph {
   }
 
   public double calculerItineraireMinimisantNombreRoutes(String cityDeparture, String cityArrival) {
+    if (!correspondanceIataCity.containsKey(cityDeparture) || !correspondanceIataCity.containsKey(
+        cityArrival)) {
+      throw new IllegalArgumentException("City not found");
+    }
 
+    Queue<City> citiesVisited = new LinkedList<>();
+    Map<City, Integer> distance = new HashMap<>();
 
-    return 0;
-  }
+    City startCity = correspondanceIataCity.get(cityDeparture);
+    citiesVisited.add(startCity);
+    distance.put(startCity, 0);
 
-  public double calculerItineraireMinimisantKm(String cityDeparture, String cityArrival) {
-    // TODO Auto-generated method stub
-    return 0;
+    while (!citiesVisited.isEmpty()) {
+      City currentCity = citiesVisited.poll();
+      int currentDistance = distance.get(currentCity);
+
+      if (currentCity.getName().equals(cityArrival)) {
+        return currentDistance;
+      }
+
+      for (Road road : roadsList) {
+        if (road.getIdSource() == currentCity.getId()) {
+          City nextCity = correspondanceIataCity.get(road.getIdDestination());
+          if (!distance.containsKey(nextCity)) {
+            citiesVisited.add(nextCity);
+            distance.put(nextCity, currentDistance + 1);
+          }
+        }
+      }
+
+      System.out.println("No route found between the two cities.");
+      return -1;
+    }
+    return  0;
+
+//    public double calculerItineraireMinimisantKm (String cityDeparture, String cityArrival){
+//      // TODO Auto-generated method stub
+//      return 0;
+//    }
   }
 }
--- src/Road.java
+++ src/Road.java
@@ -1,11 +1,26 @@
 public class Road {
 
-  private final int idSource;
-  private final int idDestination;
+  private int idSource;
+  private int idDestination;
 
   public Road(int idSource, int idDestination) {
     this.idSource = idSource;
     this.idDestination = idDestination;
   }
 
+  public int getIdSource() {
+    return idSource;
+  }
+
+  public void setIdSource(int idSource) {
+    this.idSource = idSource;
+  }
+
+  public int getIdDestination() {
+    return idDestination;
+  }
+
+  public void setIdDestination(int idDestination) {
+    this.idDestination = idDestination;
+  }
 }
